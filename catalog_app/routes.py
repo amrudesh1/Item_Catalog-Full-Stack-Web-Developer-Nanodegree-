c9ed7e12faad742c27d790adee08d4ebfaf1add7
Amrudesh Balakrishnan <amrbal@gmail.com>
2019-10-08 15:44:00 +0530

    Added Google Sign In Integration Updated Requirements

--- catalog_app/routes.py
+++ catalog_app/routes.py
@@ -1,13 +1,28 @@
 import os
+import random
+import string
+import json
+
+import httplib2
+
 from catalog_app import app, bcrypt
 from catalog_app.forms import RegistrationForm, LoginForm
 from catalog_app.models import User, Categories, Items, session
 from flask_login import login_user, current_user, logout_user
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request, make_response
+from flask import session as login_session
+from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
+import requests
 
 categories = [
     'Soccer', 'BasketBall', 'BaseBall', 'Frisbee', 'Snowboarding'
 ]
+
+GOOGLE_CLIENT_ID = '668405259059-sb3qfvqp75r1af4s578p181kbo3lvucs.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'gXlzEAqVzRf7_SK8TcSDLoDm'
+GOOGLE_DISCOVERY_URL = (
+    "https://accounts.google.com/.well-known/openid-configuration"
+)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -22,7 +37,11 @@ def user_login():
             return redirect(url_for('homeMain'))
         else:
             flash(f'Login Not Success!', 'danger')
-    return render_template('login.html', title='Login', form=form)
+
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
+    login_session['state'] = state
+    return render_template('login.html', title='Login', form=form, STATE=state, client_id=GOOGLE_CLIENT_ID)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -49,7 +68,117 @@ def homeMain():
                            current_user=current_user)
 
 
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+    print(code)
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+        # Verify that the access token is used for the intended user.
+    google_id = credentials.id_token['sub']
+    if result['user_id'] != google_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+        # Verify that the access token is valid for this app.
+    if result['issued_to'] != GOOGLE_CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print("Token's client ID does not match app's.")
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_google_id = login_session.get('google_id')
+    if stored_access_token is not None and google_id == stored_google_id:
+        response = make_response(
+            json.dumps('Current user is already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    login_session['access_token'] = credentials.access_token
+    login_session['google_id'] = google_id
+
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    if "name" in data:
+        login_session['username'] = data['name']
+    else:
+        name_corp = data['email'][:data['email'].find("@")]
+        login_session['username'] = name_corp
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+    user_id = get_user_id(data["email"])
+    if not user_id:
+        user_id = create_user(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += "Hi" + login_session['username']
+    return output
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('homeMain'))
+
+
+def get_user_id(email):
+    """Get user ID by email.
+    Argument:
+        email (str) : the email of the user.
+    """
+
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+def create_user(login_session):
+    """Crate a new user.
+    Argument:
+    login_session (dict): The login session.
+    """
+
+    new_user = User(
+        name=login_session['username'],
+        email=login_session['email'],
+    )
+    session.add(new_user)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
